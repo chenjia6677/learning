612fdf36c266c5c86e6f2f404333278e6b9ad4a1
chenjia6677 <chenjia6677@126.com>
2025-02-21 00:31:33 +0800

    工具箱

--- tools/loop.py
+++ tools/loop.py
@@ -1,3 +1,13 @@
+from typing import List, Set, Any, Tuple
 
-def test():
-    return 115
+
+def nested_loop(source: List[Set[Any]], current: List[Any] = None, index: int = 0) -> List[Tuple[Any]]:
+    """
+    多层嵌套循环
+    :param source: 源数据
+    :param current: 当前分配情况
+    :param index: 当前分配的位置
+    :return: 所有可能的分配方案
+    """
+    if current is None:
+        current = [None] * len(source)

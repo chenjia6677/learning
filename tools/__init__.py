207c50c85e126d9771eea4e5ee9d0bda9a5fefc9
chenjia6677 <chenjia6677@126.com>
2025-03-02 21:33:39 +0800

    python标准库

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,3 +1,6 @@
 from .loop import nested_loop
 from .decorator import timer
 from .self_test import FunctionTest, PerformanceTest
+
+import sys
+sys.set_int_max_str_digits(0)
--- tools/decorator.py
+++ tools/decorator.py
@@ -6,7 +6,8 @@ def timer(func):
         start = time.time()
         result = func(*args, **kwargs)
         end = time.time()
-        # print(f'{func.__name__} 执行时间：{(end - start) * 1000:.2f} 毫秒')
-        return result, f'{(end - start) * 1000:.2f} '
+        print(f'{func.__name__} 执行时间：{(end - start) * 1000:.2f} 毫秒')
+        return result
     return wrapper
+
 
